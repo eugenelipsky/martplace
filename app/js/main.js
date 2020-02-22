9a20ba95933d97d394d5f4d0f9baefc3ccd6c852
eugenelipsky <zvlipskiy@mail.ru>
2020-02-23 01:17:03 +0200

    responsive footer added

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,12 @@ $(function(){
 
  });
 
- var mixer = mixitup('.newest__items');
+ $('.menu__btn').on('click', function () {
+   $('.menu__list').slideToggle();
+ })
+
+ var mixer = document.querySelector(".newest__items");
+ if (mixer) {
+  mixer = mixitup('.newest__items');
+ }
 });
